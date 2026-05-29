7c7f97259a83ff413387c200f6115d7b37438289
Paybit-BinaryXchange.com <baileybill649@gmail.com>
2026-05-29 16:59:19 +0100

    Update home.php

--- home.php
+++ home.php
@@ -141,7 +141,7 @@ function googleTranslateElementInit() {
                         <a data-scroll href="Dashboard/login.php">Login</a>
                     </li>
                     <li class="quote" style="color:white;border-radius:5px">
-                        <a href="Dashboard/register,.php" style="color:white">Get Started</a>
+                        <a href="Dashboard/register.php" style="color:white">Get Started</a>
                     </li>
                             </ul>
         </div>
